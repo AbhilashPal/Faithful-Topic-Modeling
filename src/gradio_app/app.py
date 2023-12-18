ccc102acc0576420c10c31d87c280816cc0251d9
Abhilash Pal <abhilashpal8@gmail.com>
2023-12-18 14:37:03 +0000

    Add Some Results

--- src/gradio_app/app.py
+++ src/gradio_app/app.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from src.gradio_app.create_graphs import create_graph_comprehensiveness
 
 def plot(dataset, topic, plot_type):
-    base = f"results/Comprehensiveness/{dataset}"
+    base = f"results/keybert/{dataset}"
     plot_map = {
         "Total_Changes":1, "Topic_Changes":2, "Topic_To_Noise":3, "All_To_Noise":4, "Centroid_Movement" : 5
     } 
@@ -16,7 +16,7 @@ def plot(dataset, topic, plot_type):
 inputs = [
     gr.Dropdown(["nyt", "20newsgroup", "wiki"], label="Dataset"),
     gr.Slider(minimum=1, maximum=100, step=1, label="Topic"),
-    gr.Dropdown(["Total_Changes", "Topic_Changes", "Topic_To_Noise", "All_To_Noise"], label="Plot Type")
+    gr.Dropdown(["Total_Changes", "Topic_Changes", "Topic_To_Noise", "All_To_Noise", "Centroid_Movement"], label="Plot Type")
 ]
 outputs = gr.Image(label="Plot")
 gr.Interface(fn=plot, inputs=inputs, outputs=outputs, theme="soft").launch(share=True)
